bd767b0aae01047622931a6508bac6442a1f7961
Luis Fernando Mendoza <goliat8903@gmail.com>
2017-12-18 19:07:04 -0600

    Add route that matches a regexp

--- static-file-server/app.js
+++ static-file-server/app.js
@@ -9,6 +9,12 @@ app.use(morgan("short"));
 var staticPath = path.join(__dirname, "static");
 app.use(express.static(staticPath));
 
+app.get(/^\/users\/(\d+)$/, function(req, res) {
+  // parse paramenter as int
+  var userId = parseInt(req.params[0], 10);
+  res.end("Valid user: " + userId);
+})
+
 app.use(function(req, res) {
   res.status(404);
   res.send("File not found");
